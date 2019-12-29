f297543da310c8200e63beae584bb4bc9f2d09d7
Ahmed <HobbytronicsPK@users.noreply.github.com>
2019-12-29 21:21:03 +0500

    Add files via upload

---
+++ examples/vl53l0x_add_i2C_pins/vl53l0x_add_i2C_pins.ino
@@ -0,0 +1,44 @@
+//define new pins i.e. SDA and SCL by calling setAddress(sda_pin,scl_pins) function
+
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(21, INPUT); // Set GPIO1 on VL53L0X as INPUT
+  pinMode(22, OUTPUT); // Define Shutdown Pin
+  digitalWrite(22, HIGH); //Set Shutdown Pin to HIGH to turn the sensor ON
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL53L0X test");
+  
+  lox.setAddress(17,16);
+  
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power 
+  Serial.println(F("VL53L0X API Simple Ranging example\n\n")); 
+}
+
+
+void loop() {
+  VL53L0X_RangingMeasurementData_t measure;
+    
+  Serial.print("Reading a measurement... ");
+  lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
+
+  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println(" out of range ");
+  }
+    
+  delay(100);
+}
